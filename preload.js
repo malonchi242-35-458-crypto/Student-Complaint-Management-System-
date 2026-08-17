8ac1dba7b10cdf3130eccca8b9f98abcadab1941
malonchi242-35-458-crypto <malonchi242-35-458@diu.edu.bd>
2026-08-18 03:43:04 +0600

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("complaintAPI", {
-  getComplaints: () => ipcRenderer.invoke("get-complaints"),
-  addComplaint: (data) => ipcRenderer.invoke("add-complaint", data),
-  updateComplaint: (data) => ipcRenderer.invoke("update-complaint", data),
-  setStatus: (id, status) => ipcRenderer.invoke("set-status", { id, status }),
-  deleteComplaint: (id) => ipcRenderer.invoke("delete-complaint", id)
-});
